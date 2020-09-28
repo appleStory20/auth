71b5b2c64a3cdeca8ac1857438041989bd303dd3
appleStory20 <py.hotz2019@gmail.com>
2020-09-28 17:44:38 +0900

    09/28

---
+++ Dcrew/app/src/main/java/g/a/dcrew/LoginUser.java
@@ -0,0 +1,44 @@
+package g.a.dcrew;
+
+public class LoginUser {//로그인한 사용자 정보
+
+    private static String account;
+    private static String nickname;
+    //singleton instance
+    private static LoginUser loginUser;
+
+    private LoginUser() {
+
+    }
+
+    private LoginUser(String account, String nickname) {
+        this.account = account;
+        this.nickname = nickname;
+    }
+
+    public static LoginUser getInstance() {
+        return loginUser;
+    }
+
+    public static LoginUser initInstance(String account, String nickname){
+        loginUser = new LoginUser(account, nickname);
+        return loginUser;
+    }
+
+
+
+    public static String getAccount() {
+        return account;
+    }
+
+    public static String getNickname() {
+        return nickname;
+    }
+
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+
+}
